7c6c254bdb70fe1330fb7cece834c61b0e5361db
Latchezar Tzvetkoff <latchezar@tzvetkoff.net>
2013-04-24 13:43:45 +0300

    more and more niceness

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,26 +1,56 @@
 window.onload = function() {
-  // Set default options
   marked.setOptions({
     gfm: true,
     tables: true,
     breaks: true,
     pedantic: false,
-    sanitize: true,
+    sanitize: false,
     smartLists: true,
-    langPrefix: 'language-',
+    langPrefix: 'language-'/*,
     highlight: function(code, lang) {
       if (lang === 'js') {
         return highlighter.javascript(code);
       }
       return code;
     }
+    */
   });
 
-  var source = document.getElementById('source');
-  var markdown = document.getElementById('markdown');
+  var source = document.getElementById('editor_code');
+  var output = document.getElementById('preview_code');
+
+  source.onkeydown = function(event) {
+    var keyCode = event.keyCode ? event.keyCode : event.charCode ? event.charCode : event.which;
+    if (keyCode == 9 && !event.shiftKey && !event.ctrlKey && !event.altKey) {
+      var scrollTop = this.scrollTop;
+
+      if (this.setSelectionRange) {
+        var selectionStart = this.selectionStart;
+        var selectionEnd = this.selectionEnd;
+        this.value = this.value.substring(0, selectionStart) + '\t' + this.value.substr(selectionEnd);
+        this.setSelectionRange(selectionStart + 1, selectionStart + 1);
+        this.focus();
+      } else if (this.createTextRange) {
+        document.selection.createRange().text = '\t';
+        event.returnValue = false;
+      }
+
+      this.scrollTop = scrollTop;
+
+      if (event.preventDefault) {
+        event.preventDefault();
+      }
+
+      return false;
+    }
+
+    return true;
+  }
 
   source.onchange = source.onkeyup = function() {
     var html = marked(this.value);
-    markdown.innerHTML = '<div class="markdown-body">'+html+'</div>';
+    output.innerHTML = '<div class="markdown-body">'+html+'</div>';
   }
+
+  source.onchange();
 };
